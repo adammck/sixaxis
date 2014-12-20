73245c425e8d3976552b7978c340c87a3e8864ad
Adam Mckaig <adam.mckaig@gmail.com>
2014-12-20 17:32:55 -0500

    Refactor all the things

--- sixaxis.go
+++ sixaxis.go
@@ -1,47 +1,49 @@
-package main
+package sixaxis
 
 import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"os"
 	"strings"
 )
 
 const (
 
 	// Event Types
-	tButton = 1
+	tDigital = 1
+	tAnalog = 3
 
-	// Button Codes
+	// Digital event codes 0 or 1
 	bcSelect   = 288
 	bcL3       = 289
 	bcR3       = 290
 	bcStart    = 291
-	bcUp       = 292
-	bcRight    = 293
-	bcDown     = 294
-	bcLeft     = 295
-	bcL2       = 296 // TODO (adammck): L2 and R2 are analog buttons, but only
-	bcR2       = 297 //                 seem to be reported as 0 or 1.
-	bcL1       = 298
-	bcR1       = 299
-	bcTriangle = 300
-	bcCircle   = 301
-	bcCross    = 302
-	bcSquare   = 303
 	bcPS       = 304
 
-	// Analog Stick Codes
+	// Analog sticks: -128 to +127
 	LeftStickX  = 0
 	LeftStickY  = 1
 	RightStickX = 2
 	RightStickY = 3
 
-	// Gyro event codes
-	GyroX = 4 // tilt left/right
-	GyroY = 5 // tilt forwards/backwards
+	// Gyroscope ... ?
+	GyroX = 4 // left/right
+	GyroY = 5 // forwards/backwards
 	GyroZ = 6 // ???
+
+	// Analog buttons: 0 to 255
+	aUp    = 8
+	aRight = 9
+	aDown  = 10
+	aLeft  = 11
+	aL2 = 12
+	aR2 = 13
+	aL1 = 14
+	aR1 = 15
+	aTriangle = 26
+	aCircle = 27
+	aCross = 28
+	aSquare = 29
 )
 
 // https://github.com/torvalds/linux/blob/master/include/uapi/linux/time.h#L15
@@ -84,28 +86,31 @@ func (o *Orientation) String() string {
 }
 
 type SA struct {
-	r          io.Reader
+	r io.Reader
 
-	// Buttons
+	// Digital Buttons
 	Select   bool
 	L3       bool
 	R3       bool
 	Start    bool
-	Up       bool
-	Right    bool
-	Down     bool
-	Left     bool
-	L2       bool
-	R2       bool
-	L1       bool
-	R1       bool
-	Triangle bool
-	Circle   bool
-	Cross    bool
-	Square   bool
 	PS       bool
 
+	// Analog buttons: 0-255
+	Up    int32
+	Right int32
+	Down  int32
+	Left  int32
+	L2    int32
+	R2    int32
+	L1    int32
+	R1    int32
+	Triangle int32
+	Circle   int32
+	Cross    int32
+	Square   int32
+
 	// Sticks
+	// TODO: Should these just be LX/LY/RX/RY?
 	LeftStick  *AnalogStick
 	RightStick *AnalogStick
 
@@ -115,87 +120,101 @@ type SA struct {
 
 func New(reader io.Reader) *SA {
 	return &SA{
-		r:          reader,
-		LeftStick:  &AnalogStick{},
-		RightStick: &AnalogStick{},
+		r:           reader,
+		LeftStick:   &AnalogStick{},
+		RightStick:  &AnalogStick{},
 		Orientation: &Orientation{},
 	}
 }
 
+// String returns the current state of the controller as a string.
 func (sa *SA) String() string {
+	s := make([]string, 0, 30)
+
+	// sticks
+	if sa.LeftStick.X != 0 {
+		s = append(s, fmt.Sprintf("LX=%+04d", sa.LeftStick.X))
+	}
+	if sa.LeftStick.Y != 0 {
+		s = append(s, fmt.Sprintf("LY=%+04d", sa.LeftStick.Y))
+	}
+	if sa.RightStick.X != 0 {
+		s = append(s, fmt.Sprintf("RX=%+04d", sa.RightStick.X))
+	}
+	if sa.RightStick.Y != 0 {
+		s = append(s, fmt.Sprintf("RY=%+04d", sa.RightStick.Y))
+	}
 
 	// dpad
-	dpad := make([]string, 0, 4)
-	if sa.Up {
-		dpad = append(dpad, "up")
+	if sa.Up > 0 {
+		s = append(s, fmt.Sprintf("up=%d", sa.Up))
 	}
-	if sa.Down {
-		dpad = append(dpad, "down")
+	if sa.Down > 0 {
+		s = append(s, fmt.Sprintf("down=%d", sa.Down))
 	}
-	if sa.Left {
-		dpad = append(dpad, "left")
+	if sa.Left > 0 {
+		s = append(s, fmt.Sprintf("left=%d", sa.Left))
 	}
-	if sa.Right {
-		dpad = append(dpad, "right")
+	if sa.Right >0 {
+		s = append(s, fmt.Sprintf("right=%d", sa.Right))
 	}
 
-	// other buttons
-	buttons := make([]string, 0, 13)
+	// other analogs
+	if sa.L1 > 0 {
+		s = append(s, fmt.Sprintf("L1=%d", sa.L1))
+	}
+	if sa.L2 > 0 {
+		s = append(s, fmt.Sprintf("L2=%d", sa.L2))
+	}
+	if sa.R1 > 0 {
+		s = append(s, fmt.Sprintf("R1=%d", sa.R1))
+	}
+	if sa.R2 > 0 {
+		s = append(s, fmt.Sprintf("R2=%d", sa.R2))
+	}
+	if sa.Triangle > 0 {
+		s = append(s, fmt.Sprintf("T=%d", sa.Triangle))
+	}
+	if sa.Circle > 0 {
+		s = append(s, fmt.Sprintf("C=%d", sa.Circle))
+	}
+	if sa.Cross > 0 {
+		s = append(s, fmt.Sprintf("X=%d", sa.Cross))
+	}
+	if sa.Square > 0 {
+		s = append(s, fmt.Sprintf("S=%d", sa.Square))
+	}
+
+	// digital buttons
 	if sa.Select {
-		buttons = append(buttons, "select")
+		s = append(s, "select")
 	}
 	if sa.L3 {
-		buttons = append(buttons, "L3")
+		s = append(s, "L3")
 	}
 	if sa.R3 {
-		buttons = append(buttons, "R3")
+		s = append(s, "R3")
 	}
 	if sa.Start {
-		buttons = append(buttons, "start")
-	}
-	if sa.L2 {
-		buttons = append(buttons, "L2")
-	}
-	if sa.R2 {
-		buttons = append(buttons, "R2")
-	}
-	if sa.L1 {
-		buttons = append(buttons, "L1")
-	}
-	if sa.R1 {
-		buttons = append(buttons, "R1")
-	}
-	if sa.Triangle {
-		buttons = append(buttons, "triangle")
-	}
-	if sa.Circle {
-		buttons = append(buttons, "circle")
-	}
-	if sa.Cross {
-		buttons = append(buttons, "cross")
-	}
-	if sa.Square {
-		buttons = append(buttons, "square")
+		s = append(s, "start")
 	}
 	if sa.PS {
-		buttons = append(buttons, "PS")
+		s = append(s, "PS")
 	}
 
-	return fmt.Sprintf(
-		"&Sixaxis{Gyro=%s, L[%s] R[%s] dpad[%s] buttons[%s]}",
-		sa.Orientation,
-		sa.LeftStick,
-		sa.RightStick,
-		strings.Join(dpad, ", "),
-		strings.Join(buttons, ", "))
+	return fmt.Sprintf("&Sixaxis{%s}", strings.Join(s, ", "))
 }
 
+// Update changes the state of the controller to reflect the changes in an
+// input event. This should be called every time an input event is received.
 func (sa *SA) Update(event *inputEvent) {
 	switch event.Type {
 	case 0:
-		// do nothing?
+		// Zero events show up all the time, but never contain any codes or
+		// values. I'm guessing that they're sent when the driver has nothing
+		// useful to say. So we ignore them.
 
-	case tButton:
+	case tDigital:
 		v := buttonToBool(event.Value)
 
 		switch event.Code {
@@ -211,52 +230,17 @@ func (sa *SA) Update(event *inputEvent) {
 		case bcStart:
 			sa.Start = v
 
-		case bcUp:
-			sa.Up = v
-
-		case bcRight:
-			sa.Right = v
-
-		case bcDown:
-			sa.Down = v
-
-		case bcLeft:
-			sa.Left = v
-
-		case bcL2:
-			sa.L2 = v
-
-		case bcR2:
-			sa.R2 = v
-
-		case bcL1:
-			sa.L1 = v
-
-		case bcR1:
-			sa.R1 = v
-
-		case bcTriangle:
-			sa.Triangle = v
-
-		case bcCircle:
-			sa.Circle = v
-
-		case bcCross:
-			sa.Cross = v
-
-		case bcSquare:
-			sa.Square = v
-
 		case bcPS:
 			sa.PS = v
 
 		default:
-			fmt.Println("Unknown button code")
-			printEvent(event)
+			// There are a lot of events which we ignore here, because they're
+			// digital representations of the analog buttons. I guess the driver
+			// provides these for clients which don't support analog? They're no
+			// use to us, anyway.
 		}
 
-	// gyro? analog?
-	case 3:
+	case tAnalog:
 		switch event.Code {
 		case LeftStickX:
 			sa.LeftStick.X = event.Value
@@ -270,7 +254,6 @@ func (sa *SA) Update(event *inputEvent) {
 		case RightStickY:
 			sa.RightStick.Y = event.Value
 
-
 		case GyroX:
 			sa.Orientation.X = event.Value
 
@@ -280,13 +263,61 @@ func (sa *SA) Update(event *inputEvent) {
 		case GyroZ:
 			sa.Orientation.Z = event.Value
 
+		case aUp:
+			sa.Up = event.Value
+
+		case aRight:
+			sa.Right = event.Value
+
+		case aDown:
+			sa.Down = event.Value
+
+		case aLeft:
+			sa.Left = event.Value
+
+		case aL2:
+			sa.L2 = event.Value
+
+		case aR2:
+			sa.R2 = event.Value
+
+		case aL1:
+			sa.L1 = event.Value
+
+		case aR1:
+			sa.R1 = event.Value
+
+		case aTriangle:
+			sa.Triangle = event.Value
+
+		case aCircle:
+			sa.Circle = event.Value
+
+		case aCross:
+			sa.Cross = event.Value
+
+		case aSquare:
+			sa.Square = event.Value
+
 		default:
-			printEvent(event)
+			//fmt.Println("Unknown event code!")
+			//printEvent(event)
 		}
 
 	default:
-		fmt.Println("Unknown Event.Type")
-		printEvent(event)
+		//fmt.Println("Unknown Event.Type")
+		//printEvent(event)
+	}
+}
+
+// Run loops forever, keeping the state of the controller up to date. This
+// should be called in a goroutine.
+func (sa *SA) Run() {
+	var event inputEvent
+
+	for {
+		binary.Read(sa.r, binary.LittleEndian, &event)
+		sa.Update(&event)
 	}
 }
 
@@ -294,26 +325,7 @@ func buttonToBool(value int32) bool {
 	return value == 1
 }
 
+
 func printEvent(event *inputEvent) {
 	fmt.Printf("type=%04d, code=%04d, value=%08d\n", event.Type, event.Code, event.Value)
-}
-
-func main() {
-
-	// open the device
-	f, err := os.Open("/dev/input/event0")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	var event inputEvent
-	sa := New(f)
-
-	// Read events forever
-	for {
-		binary.Read(sa.r, binary.LittleEndian, &event)
-		sa.Update(&event)
-		fmt.Println(sa)
-	}
 }
